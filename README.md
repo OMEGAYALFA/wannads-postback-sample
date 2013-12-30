65bb3295c802dd9ac3d6b6c3c4714b036f9514ca
Sergio <sergio@innovativehall.com>
2013-12-30 12:02:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Wannads Postback samples
---
+++ php-postback.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$secret = ""; // check your app info at www.wannads.com
+
+
+$userId = isset($_GET['subId']) ? $_GET['subId'] : null;
+$transactionId = isset($_GET['transId']) ? $_GET['transId'] : null;
+$points = isset($_GET['reward']) ? $_GET['reward'] : null;
+$signature = isset($_GET['signature']) ? $_GET['signature'] : null;
+$action = isset($_GET['status']) ? $_GET['status'] : null;
+$ipuser = isset($_GET['userIp']) ? $_GET['userIp'] : "0.0.0.0";
+
+// validate signature
+if (md5($userId.$transactionId.$points.$secret) != $signature){
+    echo "ERROR: Signature doesn't match";
+    return;
+}
+
+if(isNewTransaction($transactionId)){ // Check if the transaction is new
+    processTransaction($userId, $points, $transactionId);
+    echo "OK";
+}else{
+    // If the transaction already exist please echo DUP.
+    echo "DUP";
+}
+
+?>
